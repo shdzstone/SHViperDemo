b732f8a6220e874ec5acf3ab7cbe06ff7bb7ddd5
cdz's MacBookPro <shdzstone@gmail.com>
2017-06-29 22:55:28 +0800

    btn添加

--- QObjcTour/QObjcTour/ViewController.m
+++ QObjcTour/QObjcTour/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    UIButton *_pressBtn;
+}
 
 @end
 
@@ -17,13 +19,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _pressBtn = [[UIButton alloc] initWithFrame:CGRectZero];
+    _pressBtn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:_pressBtn];
+    
 }
 
-
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    _pressBtn.frame = CGRectMake(0, 0, 100, 20);
+    _pressBtn.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma Actions
+- (void)pressBtnAction:(UIButton*)sender {
+    
+}
 
 @end
